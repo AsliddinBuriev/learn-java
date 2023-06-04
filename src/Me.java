e5c79bbb0ccb9afd36de30b091cc3d02fef947c1
Asliddin Buriev <asliddin00buriev@gmail.com>
2023-06-05 08:04:17 +0900

    my second commit

---
+++ src/Me.java
@@ -0,0 +1,10 @@
+public class Me {
+    private static int age = 15;
+    private static String name = "Asliddin";
+    public static int getAge(){
+        return age;
+    }
+    public static String getName(){
+        return name;
+    }
+}
